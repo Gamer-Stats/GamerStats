9137b1e32d89bde9923309ef6ce9b9d0b8944340
ivewor <ivewor>
2022-04-07 02:37:56 +0530

    models done also the pending ones, need test

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Wiki, WikiCategory, NewsCategory, ImageCollection, Topic
 
-# Register your models here.
+
+admin.site.register(Wiki)
+admin.site.register(WikiCategory)
+admin.site.register(NewsCategory)
+admin.site.register(ImageCollection)
+admin.site.register(Topic)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.text import slugify
 
 
 class ImageCollection(models.Model):
@@ -28,12 +30,20 @@ class BaseOptions(models.Model):
     meta_title = models.CharField(max_length=70)
     meta_description = models.TextField(blank=True)
     canonical_url = models.URLField(blank=True, null=True)
+    publish = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(BaseOptions, self).save(*args, **kwargs)
+
 
 class Topic(BaseOptions):
+    pass
+
     def __str__(self):
         return self.title
 
@@ -61,6 +71,63 @@ class NewsCategory(BaseOptions):
     intro_body = models.ForeignKey(
         Wiki, on_delete=models.PROTECT, blank=True, null=True
     )
+
+    def __str__(self):
+        return self.title
+
+
+class News(BaseOptions):
+    overview = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+    ref = models.TextField(blank=True)
+    tags = models.ManyToManyField(NewsCategory, related_name="news_tags")
+
+    def __str__(self):
+        return self.title
+
+
+class PcSpecs(BaseOptions):
+    SPECS_TYPE = (
+        ("mon", "Monitor"),
+        ("ms", "Mouse"),
+        ("msp", "Mousepad"),
+        ("key", "Keyboard"),
+        ("hs", "Headset"),
+        ("cpu", "CPU"),
+        ("gpu", "GPU"),
+        ("mb", "Motherboard"),
+        ("ram", "RAM"),
+        ("case", "Case/Tower"),
+        ("hdd", "HDD"),
+        ("ssd", "SSD"),
+        ("ps", "Power Supply"),
+        ("mic", "Microphone"),
+        ("cam", "Camera"),
+        ("wcam", "Webcam"),
+        ("ch", "Chair"),
+        ("lc", "Liquid Cooling"),
+        ("fn", "Fans"),
+        ("arm", "ARM"),
+        ("sl", "Studio Light"),
+        ("con", "Controller"),
+        ("o", "Other"),
+    )
+    specs_type = models.CharField(max_length=4, choices=SPECS_TYPE, default="o")
+    amazon_url = models.URLField()
+    reviews = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SetupSettings(BaseOptions):
+    overview = models.TextField(blank=True)
+    settings_body = models.TextField(blank=True)
+    settings = ArrayField(models.CharField(max_length=30, blank=True))
+    setup_body = models.TextField(blank=True)
+    specs = models.ManyToManyField(PcSpecs, related_name="person_specs")
+    ref = models.TextField(blank=True)
+    tags = models.ManyToManyField(Wiki, related_name="things_linked")
 
     def __str__(self):
         return self.title
