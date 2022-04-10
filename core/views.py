7f9ab2a40ae49777dfd8948b62c60d3a6fc46c1b
IVEWOR <realblackmario@gmail.com>
2022-04-11 02:05:27 +0530

    templated index.html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from core.models import SetupSettings, Wiki
 
 
 def index(request):
+    ss = SetupSettings.objects.all()[:9]
+    wikis = Wiki.objects.all()[:3]
     template_name = "index.html"
-    return render(request, template_name)
+    context = {"ss": ss, "wikis": wikis}
+    return render(request, template_name, context)
