924a7b368cceffdf4ad2344d4a04646ac6a4aa47
IVEWOR <howdy@deepslog.com>
2022-04-26 22:45:20 +0530

    Index Page Done

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,11 @@ from core import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("news/", views.news, name="news"),
-    path("news/<slug:slug>/", views.news_filter, name="news_filter"),
+    path("news/<slug:slug>/", views.news_single, name="news_single"),
+    path("news/single/<slug:slug>/", views.news_filter, name="news_filter"),
     path("setup/", views.setup, name="setup"),
     path("setup/<slug:slug>/", views.setup_single, name="setup_single"),
     path("setup/<url_type>/<slug:slug>/", views.setup_filter, name="setup_filter"),
-    path("wiki/", views.wiki_archives, name="wiki_archives"),
+    path("wiki/", views.wiki, name="wiki"),
     path("wiki/<slug:slug>/", views.wiki_single, name="wiki_single")
 ]
--- core/views.py
+++ core/views.py
@@ -7,15 +7,17 @@ from django.core.exceptions import ObjectDoesNotExist
 from core.models import News, NewsCategory, SetupSettings, Wiki, WikiCategory
 
 
+# Homepage
 def index(request):
     ss = SetupSettings.objects.all()[:9]
-    wikis = Wiki.objects.all()[:3]
+    wikis = Wiki.objects.all().exclude(Q(page_type=3) | Q(page_type=4))[:3]
     news = News.objects.all()[:7]
     template_name = "index.html"
     context = {"ss": ss, "wikis": wikis, "news": news}
     return render(request, template_name, context)
 
 
+# News Main
 def news(request):
     news = News.objects.all()
     template_name = "news_cat.html"
@@ -27,6 +29,16 @@ def news(request):
     return render(request, template_name, context)
 
 
+# News Single
+def news_single(request, slug):
+    news = News.objects.get(slug=slug)
+
+    template_name = "news_setup.html"
+    context = {"news": news}
+    return render(request, template_name, context)
+
+
+# News Tags/Category
 def news_filter(request, slug):
     tags = get_object_or_404(NewsCategory, slug=slug)
     news = News.objects.filter(tags=tags)
@@ -39,6 +51,7 @@ def news_filter(request, slug):
     return render(request, template_name, context)
 
 
+# Setup Main
 def setup(request):
     setups = SetupSettings.objects.all()
     paginator = Paginator(setups, 20)
@@ -52,13 +65,12 @@ def setup(request):
     return render(request, template_name, context)
 
 
+# Setup Tags/Category
 def setup_filter(request, slug, url_type):
     obj = get_object_or_404(Wiki, Q(slug=slug) & (Q(page_type=3) | Q(page_type=4)))
     url_type = obj.page_type
-    players_cat = SetupSettings.objects.filter(Q(game=obj) | Q(team=obj)).distinct()[
-        0:16
-    ]
-    paginator = Paginator(players_cat, 20)
+    players = SetupSettings.objects.filter(Q(game=obj) | Q(team=obj)).distinct()
+    paginator = Paginator(players, 20)
 
     page_num = request.GET.get("page")
     players_cat = paginator.get_page(page_num)
@@ -67,6 +79,7 @@ def setup_filter(request, slug, url_type):
     return render(request, template_name, context)
 
 
+# Setup Single
 def setup_single(request, slug):
     obj = get_object_or_404(SetupSettings, slug=slug)
     if obj.game.title == "Counter-Strike: Global Offensive":  # type: ignore
@@ -91,16 +104,18 @@ def setup_single(request, slug):
     return render(request, template_name, context)
 
 
-def wiki_single(request, slug):
-    obj = get_object_or_404(Wiki, slug=slug)
+# Wiki Main
+def wiki(request, slug):
+    obj = get_object_or_404(WikiCategory, slug=slug)
 
     template_name = "wiki_single.html"
     context = {"obj": obj}
     return render(request, template_name, context)
 
 
-def wiki_archives(request, slug):
-    obj = get_object_or_404(WikiCategory, slug=slug)
+# Wiki Single
+def wiki_single(request, slug):
+    obj = get_object_or_404(Wiki, slug=slug)
 
     template_name = "wiki_single.html"
     context = {"obj": obj}
