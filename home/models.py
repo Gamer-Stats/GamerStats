31b92fb765610b67e208396ac8d84fbce4d97434
IVEWOR <howdy@deepslog.com>
2022-08-12 00:22:14 +0530

    articles

--- home/models.py
+++ home/models.py
@@ -1,4 +1,4 @@
-from news.models import NewsPage
+from articles.models import ArticlePage
 from profiles.models import GamePage, ProfilePage
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
@@ -10,13 +10,23 @@ class HomePage(Page):
     intro = RichTextField(blank=True, null=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full"),
-        FieldPanel('body', classname="full"),
+        FieldPanel("intro", classname="full"),
+        FieldPanel("body", classname="full"),
     ]
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['news_posts'] = NewsPage.objects.select_related("owner", "featured_image", "locked_by").live().order_by('-last_published_at')[:6]
-        context['games'] = GamePage.objects.select_related("owner", "avatar").live()
-        context['players'] = ProfilePage.objects.select_related("owner", "avatar", "game", "player_country").live().order_by('first_published_at')[:15]
+        context["articles"] = (
+            ArticlePage.objects.select_related("owner", "featured_image", "locked_by")
+            .live()
+            .order_by("-last_published_at")[:6]
+        )
+        context["games"] = GamePage.objects.select_related("owner", "avatar").live()
+        context["players"] = (
+            ProfilePage.objects.select_related(
+                "owner", "avatar", "game", "player_country"
+            )
+            .live()
+            .order_by("first_published_at")[:15]
+        )
         return context
